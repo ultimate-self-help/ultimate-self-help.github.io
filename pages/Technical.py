f2df605e68b03810669e84fb778023f81e5c3656
Bob <ultimate-self-help@proton.me>
2024-03-22 12:42:25 +1100

    Technical only

--- pages/Technical.py
+++ pages/Technical.py
@@ -1,45 +1,66 @@
-import subprocess
-import sys
-import os
-import sqlite3
-from sqlite3 import Error
 import streamlit as st
 import database_control
 import pandas as pd
-from datetime import datetime
-from streamlit_option_menu import option_menu 
-
-st.header("DB test SQLite")
-
-# resp = database_control.get_items_all()
+#from datetime import datetime
 cnx = database_control.create_connection()
-df = pd.read_sql_query("SELECT * FROM items", cnx)
 
-# st.write("GET ALL ITEMS: ", df)
+# Page Configuration.
+st.set_page_config(page_title="Tech DB", layout='wide')
 
-title = st.multiselect(
-    "Select the Merchant:",
-    options=df["title"].unique(),
-    default=df["title"].unique(),
-)
 
-df_filtered = df.query(
-    "title == @title"
-)
+# DISPLAY TABLE ----------------
+try:
+    cnx = database_control.create_connection()
+    df = pd.read_sql_query("SELECT * FROM tech_support", cnx)
+    # title = st.multiselect(
+    #     "Filter by Title:",
+    #     options=df["title"].unique(),
+    #     default=df["title"].unique(),
+    # )
 
-submit = st.button("Create DB (First Time Users)")
+    category = st.multiselect(
+        "Filter by Category:",
+        options=df["category"].unique(),
+        default=df["category"].unique(),
+    )
+    df_filtered = df.query(
+        # "title == @title",
+        "category == @category"
+    )
+    st.write("Count: ", df_filtered['title'].count().round(2))
+    st.dataframe(df_filtered)
+    # st.rerun()
+
+except:
+    submit = st.button("Create DB (First Time Users)")
+    if submit:
+        database_control.create_db_and_tables()
+    st.write("DB does not exist yet! Create it first!")
+
+
+#try:
+    # title = st.multiselect(
+    #     "Select the Merchant:",
+    #     options=df["title"].unique(),
+    #     default=df["title"].unique(),
+    # )
+
+    # df_filtered = df.query(
+    #     "title == @title"
+    # )
+    #st.dataframe(df_filtered)
+
+
+title = st.text_input("Title: ")
+category = st.text_input("Category: ")
+symptom = st.text_input("Symptom: ")
+resolution = st.text_input("Resolution: ")
+
+submit = st.button('Add new row / entry.')
+
 if submit:
-    database_control.create_db_and_tables()
-    
-# Source: https://www.youtube.com/watch?v=YClmpnpszq8&list=PL7QI8ORyVSCaejt2LICRQtOTwmPiwKO2n&index=14
-if "my_input" not in st.session_state:
-    st.session_state["my_input"] = ""
+    print("TITLE ", title)
+    database_control.add_row(cnx, title, category, symptom, resolution)
+    st.success('Updated OK')
+    st.rerun()
 
-my_input = st.text_input("Input a text here", st.session_state["my_input"])
-submit = st.button("Submit")
-if submit:
-    st.session_state["my_input"] = my_input
-    st.write("You have entered: ", my_input)
-
-
-st.dataframe(df_filtered)
