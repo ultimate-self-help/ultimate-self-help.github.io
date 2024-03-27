6ef9ddcc0cff1ac687d1617e1a235382565f12f4
Bob <ultimate-self-help@proton.me>
2024-03-27 15:45:29 +1100

    Go live take 2

--- home.py
+++ home.py
@@ -4,6 +4,8 @@ from streamlit_extras.badges import badge
 def app():
      with st.container():
         colL, colM, colR = st.columns(3)
+        with colL:
+            pass
         with colM:
             st.write("Welcome to the..")
             st.markdown('''## :rainbow[Ultimate Self Help]''')
@@ -22,5 +24,7 @@ def app():
             st.write("")
             st.write("Send this link to others to invite and infom them of this site.")
             st.write("https://ultimate-self-help.onrender.com/")
-          
+        with colR:
+            pass
+        
     
